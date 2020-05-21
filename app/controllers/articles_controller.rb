582843c2201719f1f46367c897a4823d814bdd5b
Hemant Kumar Singh <hks9044907610@gmail.com>
2020-05-21 09:56:46 +0530

    impleted CRUD operations.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy] 
+
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -8,8 +10,45 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
   end
 
   def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash[:noticed] = "Article saved successfully !!!"
+      redirect_to @article
+    else
+      render "new"
+    end
   end
+
+  def update
+    if @article.update(article_params)
+      flash[:noticed]  = "Updated Successfully !!!"
+      redirect_to @article
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
