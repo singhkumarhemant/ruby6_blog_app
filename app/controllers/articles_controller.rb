ccec3724fde88ab3afa46eb1e428495ddb6a1842
Hemant Kumar Singh <hks9044907610@gmail.com>
2020-05-26 16:19:44 +0530

    implemented associations, restrict ui, admin-user functionality.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,14 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:show, :edit, :update, :destroy] 
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
   end
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -18,9 +20,9 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-
+    @article.user = current_user
     if @article.save
-      flash[:noticed] = "Article saved successfully !!!"
+      flash[:notice] = "Article saved successfully !!!"
       redirect_to @article
     else
       render "new"
@@ -29,7 +31,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:noticed]  = "Updated Successfully !!!"
+      flash[:notice]  = "Updated Successfully !!!"
       redirect_to @article
     else
       render "edit"
@@ -49,6 +51,14 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete article created by you!!!"
+      redirect_to @article
+    end
+
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_074658) do
+ActiveRecord::Schema.define(version: 2020_05_26_095153) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "blogs", force: :cascade do |t|
@@ -24,6 +25,15 @@ ActiveRecord::Schema.define(version: 2020_05_21_074658) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
